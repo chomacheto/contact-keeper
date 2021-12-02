47e71d86724180728a9723b547f2c336618b99c5
Ivan Chomakov <ivanchomakov@icloud.com>
2021-12-02 13:57:47 +0200

    master

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div
+      className="App"
+    >
+        My App
+    </div>
+  );
+}
+
+export default App;
